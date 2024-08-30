de8900473b13002bb269ce3301fd7c0c0c923b88
Joaquin Fernandez <joaq.fernandez16@gmail.com>
2024-08-30 12:34:33 -0300

    agrego dibujo el tablero

--- game/board.py
+++ game/board.py
@@ -12,6 +12,17 @@ class Board:
         self.__positions__[0][7] = Rook("BLACK") #Black
         self.__positions__[0][7] = Rook("WHITE") #White
         self.__positions__[7][7] = Rook("WHITE") #White
+    
+    def __str__(self):
+        board_str = ""
+        for row in self.__positions__:
+            for cell in row:
+                if cell is not None:
+                    board_str += str(cell)
+                else:
+                    board_str += " "
+            board_str += "\n"
+        return board_str
 
     def get_piece(self, row, col):
         return self.__positions__[row][col]
