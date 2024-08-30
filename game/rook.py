9d3f340c4947a2aa84c4fdcadb1b20d8341cf22d
Joaquin Fernandez <joaq.fernandez16@gmail.com>
2024-08-29 22:13:34 -0300

    agrego piezas y posiciones validas al rook)

--- game/rook.py
+++ game/rook.py
@@ -2,4 +2,8 @@ from piece import Piece
 
 
 class Rook(Piece):
-    ...
+    class Rook(Piece):
+        white_str = "♜"
+        black_str = "♖"
+    
+    def posiciones_validas(self, from_col, from_row, to_col, to_row):
